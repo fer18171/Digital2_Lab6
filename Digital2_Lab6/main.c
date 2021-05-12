ffb1d39b9b8e2570886b08d470f4724597bbdce9
fer18171 <66540300+fer18171@users.noreply.github.com>
2021-05-11 23:06:48 -0600

    Comentarios
    Se agregaron comentarios al codigo

--- Digital2_Lab6/main.c
+++ Digital2_Lab6/main.c
@@ -8,39 +8,38 @@
 int main(void)
 {
 
+    //Creamos las variables necesarias
     uint8_t LED = 2;
     uint8_t modo = 0;
     uint8_t status = 0;
     uint8_t status_p = 0;
     uint8_t semaforo = 0;
     uint8_t a = 0;
+    uint8_t b = 0;
+
+    //Configuracion del reloj a utilizar, en este caso se utiliza el de /4
     SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
 
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); //Se activa el puerto F
 
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3); //Se confguran como salida los LEDS
 
-    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_4);
+    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_4); //Config de entrada para boton de la tiva
         //Configurar tipo de entrada
     GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
 
     while(1)
     {
-        // Turn on the LED
-        //GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, LED);
-        // Delay for a bit
-
-        // Cycle through Red, Green and Blue LEDs
+        //Se utiliza lo siguiente para generar un antirrebote en el boton y unicamente cuando lo presione (flanco +) realiza la accion
         status_p = status;
         status = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_4);
         if (status == 0 && status_p!=0){
             semaforo = 1;
             modo = 0;
         }
-        else {
-            //semaforo = 0;
-        }
 
+
+        //Se aumenta continuamente el contador modo hasta llegqar a 4, se utiliza en el switch
         if (modo<=4){
             modo++;
         }
@@ -48,14 +47,15 @@ int main(void)
 
         }
         if (semaforo == 1){
+            //Se hace un switch de modo en donde cada modo es un modo del semaforo
         switch (modo){
-        case 1:
+        case 1: //Verde -- 8
             GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 8);
             SysCtlDelay(10000000);
             break;
-        case 2:
+        case 2:  //Verde Parpadeante -- 8 y 0
             a=0;
-            while (a<5){
+            while (a<5){ //Prpadea 5 veces, por lo que 5 veces se apaga y se enciente con un delay de por medio
                 a++;
                 GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0);
                 SysCtlDelay(1000000);
@@ -64,17 +64,25 @@ int main(void)
             }
 
             break;
-        case 3:
+        case 3: //Amarillo -- rojo+verde=2+8=10
             GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 10);
             SysCtlDelay(10000000);
             break;
-        case 4:
+        case 4: //Rojo -- 2
             GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 2);
             SysCtlDelay(10000000);
             break;
-        default:
+        default: //Finalmente se queda apagado
             GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0);
         }
         }
+    }
+}
+
+//Rutina de delay hecha a "mano"
+
+void RutinaDelay(tiempo){
+    for (b=0; b<tiempo; b++){
+
     }
 }
